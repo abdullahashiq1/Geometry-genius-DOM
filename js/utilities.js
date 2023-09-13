3c786424081d18db4a3efa04e232716316fc865d
abdullahashiq1 <ashdev491@gmail.com>
2023-09-13 23:10:23 +0600

    dynamic btn

--- js/utilities.js
+++ js/utilities.js
@@ -5,6 +5,13 @@ function geometryName(nameId){
     return elementValue;
 }
 
+// cm to m 
+function convertToMetre(geometryDigit){
+    const cm = geometryDigit;
+    const m = (cm / 100).toFixed(2);
+    return m;
+}
+
 function threeParams(firstValue, secondValue, nameId){
     const firstId = parseFloat(document.getElementById(firstValue).value);
     const secondId = parseFloat(document.getElementById(secondValue).value);
@@ -13,10 +20,20 @@ function threeParams(firstValue, secondValue, nameId){
     if(!isNaN(firstId)&& !isNaN(secondId)){
         const paramsCm = parameters + 'cm<sup>2</sup>';
         const geoName = geometryName(nameId);
+
+        const btnMetre = document.createElement('button');
+        btnMetre.textContent = 'Convert m²';
+        btnMetre.classList.add('convertM');
+
         const li = document.createElement('li');
-        li.innerHTML = geoName + " " + paramsCm;
+        li.innerHTML = `${geoName} ${paramsCm}`;
+        li.appendChild(btnMetre);
         const olId = document.getElementById('calculation-list');
         olId.appendChild(li);
+        btnMetre.addEventListener('click', function(){
+            const cmToMetre = convertToMetre(parameters);
+            JSAlert.alert("Metre: ", cmToMetre);
+        })
     }
     else{
         JSAlert.alert("Please enter numeric value or valid!");
@@ -31,10 +48,22 @@ function twoParams(firstValue, secondValue, nameId){
     if(!isNaN(firstId)&& !isNaN(secondId)){
         const paramsCm = parameters + 'cm<sup>2</sup>';
         const geoName = geometryName(nameId);
+
+        const btnMetre = document.createElement('button');
+        btnMetre.textContent = 'Convert m²';
+        btnMetre.classList.add('convertM');
+
         const li = document.createElement('li');
-        li.innerHTML = geoName + " " + paramsCm;
+        li.innerHTML =`${geoName} ${paramsCm}`;
+        li.appendChild(btnMetre);
+
         const olId = document.getElementById('calculation-list');
         olId.appendChild(li);
+        btnMetre.addEventListener('click', function(){
+            const cmToMetre = convertToMetre( parameters);
+            JSAlert.alert("Metre: ", cmToMetre);
+
+        })
     }
     else{
         JSAlert.alert("Please enter numeric value or valid!");
@@ -44,28 +73,41 @@ function twoParams(firstValue, secondValue, nameId){
 function ellipses(firstValue, secondValue, nameId){
     const firstId = parseFloat(document.getElementById(firstValue).value);
     const secondId = parseFloat(document.getElementById(secondValue).value);
-    // calculation 
-    const parameters = 3.14 * firstId * secondId;
-    if(!isNaN(firstId)&& !isNaN(secondId)){
-        const paramsCm = parameters + 'cm<sup>2</sup>';
+
+    if(!isNaN(firstId) && !isNaN(secondId)){
+        const parameters = (3.14 * firstId * secondId).toFixed(2);
+        const paramsCm = parameters + ' cm<sup>2</sup>';
+        // create btn 
+        const btnMetre = document.createElement('button');
+        btnMetre.textContent = 'Convert m²';
+        btnMetre.classList.add('convertM');
+
         const geoName = geometryName(nameId);
+
         const li = document.createElement('li');
-        li.innerHTML = geoName + " " + paramsCm;
+        li.innerHTML = `${geoName} ${paramsCm}`;
+
+        li.appendChild(btnMetre);
+
         const olId = document.getElementById('calculation-list');
         olId.appendChild(li);
-    }
-    else{
+        btnMetre.addEventListener('click', function(){
+            const cmToMetre = convertToMetre(parameters);
+            JSAlert.alert("Centemetre to Metre: ", cmToMetre);
+        })
+
+    }else{
         JSAlert.alert("Please enter numeric value or valid!");
     }
 }
 
-// cm to m 
-function convertToMetre(geometryDigit){
-    const cm = geometryDigit;
-    const m = cm / 100;
-    return m;
-}
 
-   
+  
+
+
+
+
+
+
 
 
